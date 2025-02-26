6c0f92689ac939d1e954d8503de0e3cbde1d142c
Jace <contactjacenair@gmail.com>
2025-02-26 13:10:41 +0000

    Define data types for products and connect home page to Postgres DB through Drizzle ORM

---
+++ src/db/drizzle.ts
@@ -0,0 +1,19 @@
+import { cwd } from 'node:process'
+import { loadEnvConfig } from '@next/env'
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+import * as schema from "@/db/schema";
+
+loadEnvConfig(cwd())
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
+
+const db = drizzle(pool, { schema });
+
+export default db;
+
+//export type DB = typeof db;
